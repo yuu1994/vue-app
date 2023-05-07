a88ce335fe598ff947f5faa5c90eb3007f60bdb6
yuu <heinhtun1994@gmail.com>
2023-05-07 16:33:41 +0630

    commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <h1>Hello Vue</h1>
   <ModalContent header="Login Success" content="Welcome" username="Kiyotaka" theme="success" />
+  <FooterContent/>
 </template>
 
 <script>
 import ModalContent from "./components/ModalContent.vue";
+import FooterContent from "./components/FooterContent.vue"
 export default {
   components: {
-    ModalContent,
+    ModalContent , FooterContent 
   },
 };
 </script>
---
+++ src/components/FooterContent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="">
+        <div class="footer">
+            This is footer
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default{
+
+}
+</script>
+
+<style>
+.footer {
+    position: fixed;
+    bottom: 0;
+    background-color: blue;
+    width: 100%;
+    height: 50px;
+    font-size: 2rem;
+    text-align: center;
+    font-weight: 800;
+    color: white;
+}
+</style>
